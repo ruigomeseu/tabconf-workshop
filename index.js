59e7c52be59b3177d444042fe8898efca4e3b9da
Rui Gomes <hello@ruigomes.me>
2019-02-05 17:37:40 -0800

    Creating messages

--- index.js
+++ index.js
@@ -27,7 +27,31 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
+    const messages = db.get('messages')
+        .value();
+
     return res.render('index.ejs', { messages });
+});
+
+app.post('/messages', async (req, res) => {
+    const id = uuidv4();
+    const content = req.body.message;
+    const paid = false;
+    const createdAt = Date.now();
+
+    db.get('messages')
+        .push({ id, content, paid, createdAt })
+        .write();
+
+    axios.post('https://dev-api.opennode.co/v1/charges', {
+        amount: 1,
+        description: 'Message to TABConf',
+        order_id: id,
+    }, { headers: { Authorization: '95164e77-7feb-43f1-9fb7-f5c1149d84dc' } })
+        .then(response => {
+            res.render('payment.ejs', { payreq: response.data.data.lightning_invoice.payreq });
+        })
+        .catch(error => console.log(error.response.data));
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
